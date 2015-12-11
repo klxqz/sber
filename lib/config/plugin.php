8a0c5fedfed410b0937e5ad20e95797143506fca
korob <korob@192.168.1.180>
2015-12-11 11:28:14 +0500

    Инициализация репозитория

---
+++ lib/config/plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'name' => 'Сбербанк',
+    'description' => ' Оплата банковскими картами через эквайринг Сбербанк',
+    'icon' => 'img/sber16.png',
+    'logo' => 'img/sber.gif',
+    'vendor' => '985310',
+    'version' => '1.0.1',
+    'type' => waPayment::TYPE_ONLINE,
+);
